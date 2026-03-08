018157ceb71666f5851ecfb8f6548c8c5b4f6a78
13101989 <35628219+13101989@users.noreply.github.com>
2026-03-08 16:41:05 +0200

    added new pydantic features

--- fastapi/nosql_example/main.py
+++ fastapi/nosql_example/main.py
@@ -1,27 +1,42 @@
 from database import user_collection
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, field_validator
 from bson import ObjectId
+from typing import Optional
 
 app = FastAPI()
 
 
-class User(BaseModel):
+class Tweet(BaseModel):
+    content: str
+    hashtags: list[str]
+
+
+class UserCreate(BaseModel):
     name: str
-    email: str
+    email: EmailStr
+    age: int
+    tweets: list[Tweet] | None = None
+
+    @field_validator("age")
+    def validate_age(cls, value):
+        if value < 18 or value > 100:
+            raise ValueError("Age must be between 18 and 100")
+
+        return value
 
 
-class UserResponse(User):
+class UserResponse(UserCreate):
     id: str
 
 
 @app.get("/users")
-def read_users() -> list[User]:
-    return [user for user in user_collection.find()]
+def read_users():
+    return [{**user, "_id": str(user["_id"])} for user in user_collection.find()]
 
 
 @app.post("/user")
-def create_user(user: User):
+def create_user(user: UserCreate):
     result = user_collection.insert_one(user.model_dump(exclude_none=True))
     user_response = UserResponse(id=str(result.inserted_id), **user.model_dump())
 
@@ -29,7 +44,7 @@ def create_user(user: User):
 
 
 @app.get("/user/{user_id}")
-def get_user(user_id: str):
+def get_user(user_id: str) -> UserResponse:
     db_user = user_collection.find_one(
         {"_id": ObjectId(user_id) if ObjectId.is_valid(user_id) else None}
     )
@@ -37,6 +52,7 @@ def get_user(user_id: str):
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    user_response = UserResponse(id=str(db_user["_id"]), **db_user)
+    # user_response = UserResponse(id=str(db_user["_id"]), **db_user)
+    db_user["id"] = str(db_user["_id"])
 
-    return user_response
+    return db_user
