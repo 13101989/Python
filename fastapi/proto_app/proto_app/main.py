481e75cf91d7c6e53ffece274bcf27e40bec3d2c
13101989 <valentin.damache@outlook.com>
2026-03-22 16:49:43 +0200

    created proto app

---
+++ fastapi/proto_app/proto_app/main.py
@@ -0,0 +1,55 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, Depends, HTTPException, status
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from proto_app.database import Item, SessionLocal, Base, engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/home")
+async def read_main():
+    return {"message": "Hello World"}
+
+
+def get_db_session():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+class ItemSchema(BaseModel):
+    name: str
+    color: str
+
+
+@app.post("/item", response_model=int, status_code=status.HTTP_201_CREATED)
+def add_item(item: ItemSchema, db_session: Session = Depends(get_db_session)):
+    db_item = Item(name=item.name, color=item.color)
+
+    db_session.add(db_item)
+    db_session.commit()
+    db_session.refresh(db_item)
+
+    return db_item.id
+
+
+@app.get("/item/{item_id}", response_model=ItemSchema)
+def get_item(item_id: int, db_session: Session = Depends(get_db_session)):
+    item_db = db_session.query(Item).filter(Item.id == item_id).first()
+
+    if item_db is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Item not found"
+        )
+
+    return item_db
---
+++ fastapi/proto_app/tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+from proto_app.main import app, get_db_session
+from proto_app.database import Base
+
+engine = create_engine(
+    "sqlite:///:memory:",
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+Base.metadata.create_all(bind=engine)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture
+def test_db_session():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture(scope="function")
+def test_client(test_db_session):
+    app.dependency_overrides[get_db_session] = lambda: test_db_session
+    client = TestClient(app)
+
+    yield client
